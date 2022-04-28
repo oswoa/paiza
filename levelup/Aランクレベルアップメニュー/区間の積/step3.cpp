3e846813f666a015fae3f50988189c85f7dc59e5
oswoa <osw.oao@gmail.com>
2022-04-28 14:18:56 +0100

    change 区間の積 step3.cpp

--- levelup/Aランクレベルアップメニュー/区間の積/step3.cpp
+++ levelup/Aランクレベルアップメニュー/区間の積/step3.cpp
@@ -12,7 +12,7 @@ https://paiza.jp/works/mondai/a_rank_level_up_problems/a_rank_twopointers_step3
 #include <vector>
 using namespace std;
 
-/* 
+/*
     ◆全体方針
     累積和を用いてM以上となる値を見つける
  */
@@ -51,3 +51,51 @@ int main() {
 
     return 0;
 }
+
+/*
+    ◆全体方針
+    しゃくとり法を用いてM以上となる値を見つける
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    uint32_t N, M;
+    cin >> N >> M;
+
+    vector<uint32_t> v(N, 0);
+    for (size_t i = 0; i < N; ++i)
+        cin >> v[i];
+
+    int ans = size(v) + 1; // 取り得ない値を格納しておく
+    size_t sum = 0;        // 合計値格納用
+    size_t right = 0;      // 右端制御用
+
+    for (size_t left = 0; left < size(v); ++left) {
+
+        // M を超えるまで加算する
+        while (right < size(v) && sum < M) {
+            sum += v[right];
+            ++right;
+        }
+
+        // M以上になったところで長さを取得
+        if (M <= sum)
+            ans = min(static_cast<size_t>(ans), right - left);
+
+        // 現在の合計からleftの値を引き、加算する範囲をずらす
+        sum -= v[left];
+    }
+
+    if (ans == size(v) + 1)
+        ans = -1;
+
+    // 答えを出力
+    cout << ans << endl;
+
+    return 0;
+}
+*/
